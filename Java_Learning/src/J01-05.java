db11bb2d2d45d8d7d6f4fa0e076d88121941244d
Youhei Kanebako <youheikanebako@mb2.local>
2023-06-08 11:37:30 +0900

    Second Commit

--- Java_Learning/src/J01-05.java
+++ Java_Learning/src/J01-05.java
@@ -1,6 +1,6 @@
 //Ex1_05_1
 class J01_05 {
-        public static void main(String[] args){
+    public static void main(String[] args) {
         int itLifetimeWage = 276000000;
         // long marketSize = 28534400000000L;
         // char faboliteChar = '愛';
