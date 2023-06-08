2e208f240108327039b2a14b5819b959b44c3975
Youhei Kanebako <youheikanebako@mb2.local>
2023-06-08 11:05:18 +0900

    Initial Commit

---
+++ Java_Learning/src/J01-05.java
@@ -0,0 +1,12 @@
+//Ex1_05_1
+class J01_05 {
+        public static void main(String[] args){
+        int itLifetimeWage = 276000000;
+        // long marketSize = 28534400000000L;
+        // char faboliteChar = '愛';
+        // String targetJobName = "SE";
+        // boolean isPreviousGraduate = true;
+
+        System.out.println(itLifetimeWage);
+    }
+}
